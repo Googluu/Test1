79087da48896719dea081938e4db877a3f315643
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-02-16 18:58:34 -0500

    swagger docs graphql

--- server.js
+++ server.js
@@ -9,7 +9,21 @@ const schema = require("./graphql/schema");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+
+const whitelist = [
+  "http://localhost:3000",
+  " https://my-test1uno.herokuapp.com/",
+];
+const optionsCors = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("no permitido"));
+    }
+  },
+};
+app.use(cors(optionsCors));
 
 app.get("/", (req, res) => {
   res.send("Welcome");
