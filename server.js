e5d4128b6625a421a1b44e984d830ce2a8a778c6
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-02-16 18:49:24 -0500

    swagger docs graphql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
@@ -8,6 +9,7 @@ const schema = require("./graphql/schema");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome");
